6aa25f20ba0e557b2a29c244e0eb3286d51701a8
John Martin <johndavidfive@gmail.com>
2022-09-08 07:45:46 -0700

    Initial Commit

---
+++ LengthConversion/LengthConversionApp.swift
@@ -0,0 +1,17 @@
+//
+//  LengthConversionApp.swift
+//  LengthConversion
+//
+//  Created by john martin on 9/8/22.
+//
+
+import SwiftUI
+
+@main
+struct LengthConversionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
